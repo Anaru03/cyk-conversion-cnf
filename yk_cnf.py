a9a5c389abbf2b5b47ec895eaa3d7006adacc31b
Anaru03 <dele22428@uvg.edu.gt>
2025-10-17 15:09:07 -0600

    implementacion_algoritmo_cyk

--- yk_cnf.py
+++ yk_cnf.py
@@ -18,3 +18,30 @@ def leer_gramatica_cnf(ruta_archivo):
                 prod_list.append(prod_symbols)
             grammar[var] = prod_list
     return grammar
+
+#Implementación CYK
+def cyk_parse(words, grammar):
+    n = len(words)
+    table = [[set() for _ in range(n)] for _ in range(n)]
+    back = [[dict() for _ in range(n)] for _ in range(n)]
+
+    for i, word in enumerate(words):
+        for var, productions in grammar.items():
+            for prod in productions:
+                if len(prod) == 1 and prod[0] == word:
+                    table[i][i].add(var)
+                    back[i][i][var] = word
+
+    for l in range(2, n+1):
+        for i in range(n - l + 1):
+            j = i + l - 1
+            for k in range(i, j):
+                for var, productions in grammar.items():
+                    for prod in productions:
+                        if len(prod) == 2:
+                            B, C = prod
+                            if B in table[i][k] and C in table[k+1][j]:
+                                table[i][j].add(var)
+                                back[i][j][var] = (k, B, C)
+
+    return table, back
