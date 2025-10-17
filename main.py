78dc28b753cea50c13af58714cf7b93aeab062c4
Anaru03 <dele22428@uvg.edu.gt>
2025-10-17 12:35:59 -0600

    Agrega función para reemplazar terminales y binarizar (preparar CNF)

--- main.py
+++ main.py
@@ -75,3 +75,39 @@ def eliminar_unitarias(gramatica):
                     nueva[A].append(prod)
     return nueva
 
+# Reemplazar terminales y binarizar
+contador = 1
+def nuevo_simbolo(prefijo="X"):
+    global contador
+    s = f"{prefijo}{contador}"
+    contador += 1
+    return s
+
+def preparar_cnf(gramatica):
+    nueva = defaultdict(list)
+    reemplazos = {}
+    for A in gramatica:
+        for prod in gramatica[A]:
+            if len(prod) > 2:
+                simbolos = list(prod)
+                temp = nuevo_simbolo()
+                nueva[A].append((simbolos[0], temp))
+                for i in range(1, len(simbolos) - 2):
+                    sig = nuevo_simbolo()
+                    nueva[temp].append((simbolos[i], sig))
+                    temp = sig
+                nueva[temp].append(tuple(simbolos[-2:]))
+            elif len(prod) == 2:
+                nueva[A].append(prod)
+            elif len(prod) == 1:
+                x = prod[0]
+                if x not in gramatica:
+                    if x not in reemplazos:
+                        var = nuevo_simbolo("T")
+                        reemplazos[x] = var
+                    nueva[A].append((reemplazos[x],))
+                else:
+                    nueva[A].append(prod)
+    for t, v in reemplazos.items():
+        nueva[v].append((t,))
+    return nueva
